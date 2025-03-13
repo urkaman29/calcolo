2e44a6af084ada3fd0f0f80591b02ca41944e207
urkaman29 <76437306+urkaman29@users.noreply.github.com>
2025-03-13 16:04:56 +0100

    Update progetto1.py

--- progetto1.py
+++ progetto1.py
@@ -11,10 +11,13 @@ funzione = str(input("Inserisci la funzione: "))
 f_sym = sp.sympify(funzione)
 f = sp.lambdify(x, f_sym, 'numpy')
 
+# Definisco la bisettrice
+bisettrice= sp.sympify("x")
+
 # Definisco intervallo funzione
 intervallo= []
-intervallo[0]= int(input("Inserisci a dell'intervallo: "))
-intervallo[1]= int(input("Inserisci b dell'intervallo: "))
+intervallo.append(int(input("Inserisci a dell'intervallo: ")))
+intervallo.append(int(input("Inserisci b dell'intervallo: ")))
 
 # Definisco punto di inizio x0
 x0= int(input("Inserisci x0: "))
@@ -27,10 +30,22 @@ d = sp.lambdify(x, derivata, 'numpy')
 int_func= f_sym - x
 intersezione= sp.solve(int_func, x) #lista con x dei punti di intersezione tra f(x) ed y=x.
 
+# Funzione per intersecare 
+def interseca(f1, f2):
+    funz= f1 - f2
+    intersezione= sp.solve(funz, x)
+    return intersezione[0]
+
 # Funzione per iterazione
-def iterazione(x0, f_sym):
-    tuple_punti= []
-    for i in range(120):
-        if(i == 0):
-            tuple_punti.append([x0, f(x0)])
-    return 0
+tuple_punti= []
+for i in range(120):
+    if(i == 0):
+        tuple_punti.append([x0, f(x0)])
+    else:
+        if(i%2 == 1):  #orizzontale
+            x1 = interseca(interseca(tuple_punti[i-1][1], bisettrice)) 
+            tuple_punti.append([tuple_punti[i-1][1], x1])
+        if(i%2 == 0):  #verticale
+            tuple_punti.append([tuple_punti[i-1][1], f(x1)])
+
+#20/(x**2+2*x+10)
